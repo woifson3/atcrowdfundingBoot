f4ed353c37554581ddc53391a10c42142fd58d90
woifson3 <343492832@qq.com>
2020-05-20 23:59:00 +0800

    springBoot改造众筹项目：发起众筹，响应众筹

---
+++ scw-register/src/test/java/com/atguigu/scw/ScwRegisterApplicationTests.java
@@ -0,0 +1,21 @@
+package com.atguigu.scw;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ScwRegisterApplicationTests {
+
+	@Test
+	public void contextLoads() {
+		System.out.println("this is a program");
+		System.out.println("this is a program");
+		while (true){
+			System.out.println("this is a program");
+		}
+	}
+
+}
